30e162a25e81b73102f0ed21a469fe10a9c8cb71
SurekshyaSharma <43008704+SurekshyaSharma@users.noreply.github.com>
2020-02-13 16:41:48 -0600

    Create rangefun2.py

---
+++ rangefun2.py
@@ -0,0 +1,36 @@
+
+# first expression
+for num in range(1, 11, 1):
+    print(num, end=' ')
+print("")
+print("--------------------")
+
+# Second expression
+for num in range(1, 14, 2):
+    print(num, end=' ')
+print("")
+print("----------------")
+
+# Third expression
+for num in range(47, 0, -24):
+    print(num, end=' ')
+print("")
+print("-------")
+
+# Fourth expression
+for num in range(10, 0, -1):
+    print(num, end=' ')
+print("")
+print("--------------------")
+
+# Fifth expression
+for num in range(20, -1, -4):
+    print(num, end=' ')
+print("")
+print("--------------------")
+
+# Sixth expression
+for num in range(3, 34, 6):
+    print(num, end=' ')
+print("")
+print("--------------------")
