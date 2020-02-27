d7567b57258a4f22babb4c24fc60c02db6977298
SurekshyaSharma <43008704+SurekshyaSharma@users.noreply.github.com>
2020-02-27 16:11:36 -0600

    Create test_f2c.py

---
+++ test_f2c.py
@@ -0,0 +1,42 @@
+import pytest
+import ftoc
+
+
+def test_32():
+    assert 32 == ftoc.f2c(32)
+
+#
+# def test_minus_2():
+#     assert 32 == ftoc.f2c(-2)
+#
+#
+# def test_32_to_0():
+#     assert 32 == ftoc.f2c(32)
+#
+#
+# def test_32_to_0():
+#     assert 32 == ftoc.f2c(32)
+#
+#
+# def test_32_to_0():
+#     assert 32 == ftoc.f2c(32)
+#
+#
+# def test_32_to_0():
+#     assert 32 == ftoc.f2c(32)
+#
+#
+# def test_32_to_0():
+#     assert 32 == ftoc.f2c(32)
+#
+#
+# def test_32_to_0():
+#     assert 32 == ftoc.f2c(32)
+#
+#
+# def test_32_to_0():
+#     assert 32 == ftoc.f2c(32)
+#
+#
+# def test_32_to_0():
+#     assert 32 == ftoc.f2c(32)
