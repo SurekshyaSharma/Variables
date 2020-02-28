d662dd1882090ae84ff35fdc9184c35560848fbd
SurekshyaSharma <43008704+SurekshyaSharma@users.noreply.github.com>
2020-02-28 10:39:13 -0600

    Update test_f2c.py

--- test_f2c.py
+++ test_f2c.py
@@ -3,40 +3,40 @@ import ftoc
 
 
 def test_32():
-    assert 32 == ftoc.f2c(32)
+    assert 0.0 == ftoc.f2c(32.0)
 
-#
-# def test_minus_2():
-#     assert 32 == ftoc.f2c(-2)
-#
-#
-# def test_32_to_0():
-#     assert 32 == ftoc.f2c(32)
-#
-#
-# def test_32_to_0():
-#     assert 32 == ftoc.f2c(32)
-#
-#
-# def test_32_to_0():
-#     assert 32 == ftoc.f2c(32)
-#
-#
-# def test_32_to_0():
-#     assert 32 == ftoc.f2c(32)
-#
-#
-# def test_32_to_0():
-#     assert 32 == ftoc.f2c(32)
-#
-#
-# def test_32_to_0():
-#     assert 32 == ftoc.f2c(32)
-#
-#
-# def test_32_to_0():
-#     assert 32 == ftoc.f2c(32)
-#
-#
-# def test_32_to_0():
-#     assert 32 == ftoc.f2c(32)
+
+def test_minus_32():
+    assert -35.56 == ftoc.f2c(-32.0)
+
+
+def test_100():
+    assert 37.78 == ftoc.f2c(100.0)
+
+
+def test_minus_100():
+    assert -73.33 == ftoc.f2c(-100.0)
+
+
+def test_1():
+    assert -17.22 == ftoc.f2c(1.0)
+
+
+def test_minus_1():#
+    assert -18.33 == ftoc.f2c(-1.0) #
+
+
+def test_0():
+    assert -17.78 == ftoc.f2c(0.0)
+
+
+def test_minus_0():
+    assert -17.78 == ftoc.f2c(-0.0)
+
+
+def test_300():
+    assert 148.89 == ftoc.f2c(300.0)
+
+
+def test_minus_300():
+    assert -184.44 == ftoc.f2c(-300)
