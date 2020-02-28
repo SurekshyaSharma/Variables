82f1c4f89bd5d1add70cab1206c08b982cb2e952
SurekshyaSharma <43008704+SurekshyaSharma@users.noreply.github.com>
2020-02-28 10:41:57 -0600

    Update ftoc.py
    While running the test file, main() should be commented out

--- ftoc.py
+++ ftoc.py
@@ -1,15 +1,19 @@
+
+
 def f2c(fahr):
+
     Celsius = (fahr - 32) * 5 / 9
     # Fahrenheit = 9.0/5.0 * Celsius + 32
-    Celsius = str(round(Celsius, 2))
-    fahr = str(fahr)
-    print(Celsius)
-    print(fahr + " degree F is " + Celsius + " degree C.")
+    rounded_Celsius = round(Celsius, 2)
+    result = rounded_Celsius
+    return result
 
 
 def main():
     value = float(input("Enter the  temperature in degree F: "))
-    f2c(value)
+    print(value)
+    print(f2c(value))
 
 
-main()
+# main()
+
